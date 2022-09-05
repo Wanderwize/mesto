4c215dc9931f642ffdacf9bbc372cd06d9deb839
Wanderwize~ <wanderwwwize@yandex.ru>
2022-09-05 20:52:44 +0500

    change 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,27 @@
-
-let showPopup = document.querySelector('.popup');
-let popupOpen = document.querySelectorAll('.profile__edit-btn')[0];
+let popup = document.querySelector('.popup');
 let changeName = document.querySelector('.popup__input_firstname');
-let changeLast = document.querySelector('.popup__input_lastname');
-let userName = document.querySelector('.profile__info-title');
-let userLastname = document.querySelector('.profile__info-subtitle');
-let saveInfo = document.querySelector('.popup__save-btn');
-let closePopup = document.querySelector('.popup__close')
+let userJobInput = document.querySelector('.popup__input_lastname');
+let userNameInput = document.querySelector('.profile__info-title');
+let userJob = document.querySelector('.profile__info-subtitle');
+let popupCloseButton = document.querySelector('.popup__close')
 
-popupOpen.addEventListener("click", function open() {
-  //showPopup.style.visibility = 'visible';
-  //showPopup.style.opacity = '1';
-  showPopup.classList.add('popup__open');
+function openPopup() {
+  popup.classList.add('popup__open');
+};
 
+document.querySelector('.profile__edit-btn').addEventListener("click", openPopup);
 
-});
+function closePopup() {
+  popup.classList.remove('popup__open');
+};
 
-closePopup.addEventListener("click", function closed() {
-  //showPopup.style.visibility = 'hidden';
-  //showPopup.style.opacity = '0';
-  showPopup.classList.remove('popup__open');
+popupCloseButton.addEventListener("click", closePopup);
 
-});
-
-
-saveInfo.addEventListener("click", function changeInfo(evt) {
+document.querySelector('.popup__save-btn').addEventListener("click", function changeInfo(evt) {
   evt.preventDefault();
-
-  userName.innerHTML = `<h1 class="profile__info-title">${changeName.value}</h1>`;
-  userLastname.innerHTML = `<h2 class="profile__info-subtitle">${changeLast.value}</h2>`;
-  showPopup.classList.remove('popup__open');
+  userNameInput.textContent = `${changeName.value}`;
+  userJob.textContent = `${userJobInput.value}`;
+  popup.classList.remove('popup__open');
 });
 
 
